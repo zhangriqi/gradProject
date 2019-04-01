9d2d5255c2ee032644233ed2d1bd115ad1a8ca65
zhangriqi <zhangruiqi@Riqis-MacBook.local>
2019-04-01 13:05:04 +0800

    Add comment for get_sent_aspects()

--- transformers/asp_extractors.py
+++ transformers/asp_extractors.py
@@ -8,9 +8,13 @@ class SentenceAspectExtractor():
         pass
 
     def get_sent_aspects(self, sentence):
+    	'''
+    	INPUT: a sentence object
+    	OUTPUT: a list of the features extracted from the given sentence
+    	'''
         tagged_sent = sentence.pos_tagged
         aspects = self.get_NPs(tagged_sent)
-        return aspects
+        return aspects 
 
     def get_NPs(self, tagged_sent):
         NPs = []
@@ -19,5 +23,5 @@ class SentenceAspectExtractor():
                 if wordTag[1] == "NN" or wordTag[1] == "NR" or wordTag[1] == "NT":
                     if wordTag[0] not in NPs:
                         NPs.append(str(wordTag[0]))
-        return NPs
+        return NPs 
 
