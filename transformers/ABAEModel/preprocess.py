058de1394e29483699952b7ab6a471e19e23a8d5
zhangriqi <zhangruiqi@Riqis-MacBook.local>
2019-04-02 15:47:58 +0800

    Add code for preprocessing test data

--- transformers/ABAEModel/preprocess.py
+++ transformers/ABAEModel/preprocess.py
@@ -38,7 +38,7 @@ def parseSentence(line):
     text_rmstop = [i for i in line if i not in stop]
     # text_stem = [lmtzr.lemmatize(w) for w in text_rmstop]
     # return text_rmstem
-    print(text_rmstop)
+    # print(text_rmstop)
     return text_rmstop
 
 
@@ -54,28 +54,30 @@ def preprocess_train(domain):
         	out.write(' '.join(tokens)+'\n')
 
 def preprocess_test(domain):
-    # For restaurant domain, only keep sentences with single 
-    # aspect label that in {Food, Staff, Ambience}
+    # f1 = codecs.open('../datasets/'+domain+'/test.xlsx', 'r', 'utf-8')
+    # f2 = codecs.open('../datasets/'+domain+'/test_label.xlsx', 'r', 'utf-8')
+    f1 = U.read_file('./datasets/'+domain+'/test.xlsx')
+    f2 = U.read_file('./datasets/'+domain+'/test_label.xlsx')
 
-    f1 = codecs.open('../datasets/'+domain+'/test.txt', 'r', 'utf-8')
-    f2 = codecs.open('../datasets/'+domain+'/test_label.txt', 'r', 'utf-8')
-    out1 = codecs.open('../preprocessed_data/'+domain+'/test.txt', 'w', 'utf-8')
-    out2 = codecs.open('../preprocessed_data/'+domain+'/test_label.txt', 'w', 'utf-8')
+    out1 = codecs.open('./preprocessed_data/'+domain+'/test.txt', 'w', 'utf-8')
+    out2 = codecs.open('./preprocessed_data/'+domain+'/test_label.txt', 'w', 'utf-8')
 
     for text, label in zip(f1, f2):
-        label = label.strip()
-        if domain == 'restaurant' and label not in ['Food', 'Staff', 'Ambience']:
-            continue
-        tokens = parseSentence(text)
-        if len(tokens) > 0:
-            out1.write(' '.join(tokens) + '\n')
-            out2.write(label+'\n')
+    	textList = ast.literal_eval(text)
+    	# labelList = ast.literal_eval(label)
+    	# label = label.strip()
+    	tokens = parseSentence(textList)
+    	if len(tokens) > 0:
+        	out1.write(' '.join(tokens) + '\n')
+        	out2.write(label + '\n')
+    	# if domain == 'restaurant' and label not in ['Food', 'Staff', 'Ambience']:
+        #     continue
 
 def preprocess(domain):
     print ('\t'+domain+' train set ...')
     preprocess_train(domain)
-    # print '\t'+domain+' test set ...'
-    # preprocess_test(domain)
+    print ('\t'+domain+' test set ...')
+    preprocess_test(domain)
 
 
 cleanData('2014-2015')
